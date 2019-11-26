fffc7d72d2dfa4a771b4cf9014ddef1bc745ddc5
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-26 11:53:16 -0300

    Update W5-A2-wrapup.m

--- MATLAB/Coursera - Introduction to Programming with MATLAB/W5-A2-wrapup.m
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W5-A2-wrapup.m
@@ -1,4 +1,5 @@
 %%% Week 05 - Assignment: Lesson 4 Wrap-up
+% Matrix Construction
 
 % Write a function called 'trio' that takes two positive integer inputs 'n' and 'm'. The function returns a 3n-by-m matrix called 'T'.
 % The top third of T (an n by m submatrix) is all 1s, the middle third is all 2-s while the bottom third is all 3-s.
