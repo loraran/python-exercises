3e042582bfef5160210366af21bcace112f9b06a
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-26 11:40:22 -0300

    Create W4-A1-simplefunction.m

---
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W4-A1-simplefunction.m
@@ -0,0 +1,12 @@
+%%% Week 04 - Assignment: A Simple Function
+% Write your first function.
+
+% Write a function called 'tri_area' that returns the area of a triangle with base b and height h,
+% where b and h are input arguments of the function in that order.
+
+function area = tri_area(b,h)
+    area = (b*h)/2;
+end
+
+% Test that your function runs as expected before pressing Submit
+area = tri_area(5,4)
