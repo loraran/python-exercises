8deb11d28e64bb3f5019b8953c990c41b575f001
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-26 11:28:43 -0300

    Create W02-A2-wrapup.m

---
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W02-A2-wrapup.m
@@ -0,0 +1,10 @@
+%%% Week 02 - Assignment: Lesson 1 Wrap-up
+%   1. As of early 2018, Usain Bolt holds the world record in the men's 100-meter dash. It is 9.58 seconds.
+% What was his average speed in km/h? Assign the result to a variable called 'hundred'.
+%   2. Kenyan Eliud Kipchoge set a new world record for men of 2:01:39 on September 16, 2018.
+% Assign his average speed in km/h to the variable 'marathon'. The marathon distance is 42.195 kilometers.
+
+speed100 = 100/9.58;
+hundred = speed100*3.6; disp(hundred);
+
+marathon = 42.195/2.0275; disp(marathon);
