5387f4383ac25ad02721f405b3521783708b3058
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-26 11:35:45 -0300

    Create W03-A2-matrixindexing.m

---
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W03-A2-matrixindexing.m
@@ -0,0 +1,7 @@
+%%% Week 03 - Assignment: Matrix Indexing Practice
+% Practice accessing parts of a matrix.
+% Given matrix 'A', assign the second column of A to a variable 'v'. Afterwards change each element of the last row of A to 0.
+
+A = [1:5; 6:10; 11:15; 16:20];
+v = A(:,2);
+A(end,:) = 0;
