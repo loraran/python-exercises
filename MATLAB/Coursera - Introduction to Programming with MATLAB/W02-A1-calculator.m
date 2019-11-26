3259fab94e3699ecb1fd846d2a69e4b319e14db8
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-26 11:26:05 -0300

    Create W02-A1-calculator.m

---
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W02-A1-calculator.m
@@ -0,0 +1,12 @@
+%%% Week 02 - Assignment: MATLAB as a Calculator
+% We borrowed $1000 at a 10% annual interest rate. If we did not make a payment for two years,
+% and assuming there is no penalty for non-payment, how much do we owe now?
+% Assign the result to a variable called 'debt'.
+
+debt = 1000;
+years = 2;
+for year = 1:1:years
+    rate = 0.1*debt;
+    debt = debt + rate;
+end;
+disp(debt);
