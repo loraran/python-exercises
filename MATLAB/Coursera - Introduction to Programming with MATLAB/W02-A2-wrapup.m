95e22b83f49da57fd0fba676dbcca7adc8d4ec99
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-26 11:33:51 -0300

    Update W02-A2-wrapup.m

--- MATLAB/Coursera - Introduction to Programming with MATLAB/W02-A2-wrapup.m
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W02-A2-wrapup.m
@@ -1,7 +1,7 @@
 %%% Week 02 - Assignment: Lesson 1 Wrap-up
-%   1. As of early 2018, Usain Bolt holds the world record in the men's 100-meter dash. It is 9.58 seconds.
+% 1. As of early 2018, Usain Bolt holds the world record in the men's 100-meter dash. It is 9.58 seconds.
 % What was his average speed in km/h? Assign the result to a variable called 'hundred'.
-%   2. Kenyan Eliud Kipchoge set a new world record for men of 2:01:39 on September 16, 2018.
+% 2. Kenyan Eliud Kipchoge set a new world record for men of 2:01:39 on September 16, 2018.
 % Assign his average speed in km/h to the variable 'marathon'. The marathon distance is 42.195 kilometers.
 
 speed100 = 100/9.58;
