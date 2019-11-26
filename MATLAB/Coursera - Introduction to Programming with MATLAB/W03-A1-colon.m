e6b4627eade38e965c0ef29fef71219eabc0ddeb
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-26 11:33:37 -0300

    Update W03-A1-colon.m

--- MATLAB/Coursera - Introduction to Programming with MATLAB/W03-A1-colon.m
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W03-A1-colon.m
@@ -1,7 +1,7 @@
 %%% Week 03 - Assignment: Colon Operator
 % Colon Operator Practice
-%  1. Create a vector of all the odd positive integers smaller than 100 in increasing order and save it into variable 'odds'. 
-%  2. Create a vector of all the even positive integers smaller than or equal to 100 in decreasing order and save it into variable 'evens'.
+% 1. Create a vector of all the odd positive integers smaller than 100 in increasing order and save it into variable 'odds'. 
+% 2. Create a vector of all the even positive integers smaller than or equal to 100 in decreasing order and save it into variable 'evens'.
 
 odds = 1:2:100;
 evens = 100:-2:1;
