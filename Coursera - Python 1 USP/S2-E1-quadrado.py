7f21680fdd22e61b01b5f0fe469fa64b45914114
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-27 22:57:43 -0300

    Create S2-E1-quadrado.py

---
+++ Coursera - Python 1 USP/S2-E1-quadrado.py
@@ -0,0 +1,7 @@
+# Semana 02 - Exercício 1 - quadrado.py
+# Faça um programa que receba o valor correspondente ao lado de um quadrado, calcule e imprima seu perímetro e sua área.
+
+side = input('Please inform the side of the square: ');
+x = int(side)*4;
+y = int(side)**2;
+print('perímetro:',x,'- área:',y);
