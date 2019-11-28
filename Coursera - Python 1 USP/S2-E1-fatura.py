188ac4ce89d35cd8341c592d0aa7c82c9af62896
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-27 21:30:59 -0300

    Create S2-E1-fatura.py

---
+++ Coursera - Python 1 USP/S2-E1-fatura.py
@@ -0,0 +1,8 @@
+# Exercício 1 - fatura.py
+
+nome = input('Digite o nome do cliente: ');
+diavenc = input('Digite o dia de vencimento: ');
+mesvenc = input('Digite o mês de vencimento: ');
+fatura = input('Digite o valor da fatura: ');
+print('Olá,',nome);
+print('A sua fatura com vencimento em',diavenc,'de',mesvenc,'no valor de R$',fatura,'está fechada.');
