1e1e4f72dff1ed1c8d841660590288051f427d7f
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-11-27 23:06:17 -0300

    Update S3-E1-paridade.py

--- Coursera - Python 1 USP/S3-E1-paridade.py
+++ Coursera - Python 1 USP/S3-E1-paridade.py
@@ -1,4 +1,5 @@
 # Semana 03 - Exercício 1 - paridade.py
+
 # Receba um número inteiro na entrada e imprima 'par' quando o número for par, ou 'ímpar' quando o número for ímpar.
 
 numero = int(input('Informe um número: '));
