6cd08a5897981eceeb97195312daea691ef0f46a
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2019-12-05 01:39:47 -0300

    Create S5-E1-maximo_2.py

---
+++ Python/Coursera - Python 1 USP/S5-E1-maximo_2.py
@@ -0,0 +1,9 @@
+# Semana 05 - Exercício 1 - maximo_2.py
+
+# Escreva a função 'maximo' que recebe 2 números inteiros como parâmetro e devolve o maior deles.
+
+def maximo(num1,num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
