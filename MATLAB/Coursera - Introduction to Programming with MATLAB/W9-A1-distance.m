90546daabb5ddb74f14db173245096b4882bceda
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2020-08-12 19:35:51 -0300

    Update W9-A1-distance.m

--- MATLAB/Coursera - Introduction to Programming with MATLAB/W9-A1-distance.m
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W9-A1-distance.m
@@ -1,3 +1,12 @@
+%%% Week 09 - Assignment: Excel Files
+% Practice Excel file I/O.
+
+% The attached 'Distances.xlsx' file contains a spreadsheet with the pairwise distances in miles of the top 100 US cities by population.
+% Write a function called 'get_distance' that accepts two character vector inputs representing the names of two cities.
+% The function returns the distance between them as an output argument called 'distance'.
+% If one or both of the specified cities are not in the file, the functions returns -1.
+% Your function should load the data only once. File I/O is a time consuming operation.
+
 function [distance] = get_distance(city1,city2)
     % input format : city1 = 'city1, STATE' , city2 = 'city2, STATE'
     % NOTES: City names are only to be found on column A and row 1.
@@ -17,10 +26,10 @@ function [distance] = get_distance(city1,city2)
     end
     
     if matchfound1 == false || matchfound2 == false
-        fprintf('One or both of the specified cities are not in the file.\n');
+        %fprintf('One or both of the specified cities are not in the file.\n');
         distance = -1;
     else
         num = xlsread('Distances.xlsx');
         distance = num(coord1,coord2);
-        fprintf('The distance between [%s] and [%s] is %u miles.\n',city1,city2,distance);
+        %fprintf('The distance between [%s] and [%s] is %u miles.\n',city1,city2,distance);
     end
