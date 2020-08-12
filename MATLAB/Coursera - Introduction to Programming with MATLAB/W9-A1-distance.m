0e73571dd27c76c744b55bd95ecee11040db98ca
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2020-08-12 19:11:52 -0300

    Update W9-A1-distance.m

--- MATLAB/Coursera - Introduction to Programming with MATLAB/W9-A1-distance.m
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W9-A1-distance.m
@@ -2,17 +2,15 @@ function [distance] = get_distance(city1,city2)
     % input format : city1 = 'city1, STATE' , city2 = 'city2, STATE'
     % NOTES: City names are only to be found on column A and row 1.
     
-    [~, citylist1] = xlsread('Distances.xlsx',1,'A:A');
-    [~, citylist2] = xlsread('Distances.xlsx',1,'1:1');
-    % numel(citylist1) and numel(citylist2) are the same.
+    [~, citylist] = xlsread('Distances.xlsx',1,'A:A'); % Distances is a square matrix.
     
     matchfound1 = false; matchfound2 = false;
-    for i = 1:numel(citylist1)
-        if strfind(city1,citylist1{i,1}) == 1
+    for i = 1:numel(citylist)
+        if strfind(city1,citylist{i,1}) == 1
             coord1 = i;
             matchfound1 = true;
-        end    
-        if strfind(city2,citylist2{1,i}) == 1
+        end
+        if strfind(city2,citylist{i,1}) == 1
             coord2 = i;
             matchfound2 = true;
         end
