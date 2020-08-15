a8dbcd6f1f0089a05f6d0ef02b6c5fa6b72109b7
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2020-08-14 23:16:37 -0300

    Create W9-A3-saddle.m

---
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W9-A3-saddle.m
@@ -0,0 +1,14 @@
+%%% Week 09 - Final Assignment 1: Saddle Points
+% Find the saddle points of a matrix.
+
+% Write a function called 'saddle' that finds saddle points in the input matrix 'M'. For the purposes of this problem:
+% A saddle point is defined as an element whose value is greater than or equal to every element in its row,
+% and less than or equal to every element in its column. Note that there may be more than one saddle point in 'M'.
+% Return a matrix called 'indices' that has exactly two columns.
+% Each row of indices corresponds to one saddle point with the first element of the row containing the row index of the saddle point,
+% and the second element containing the column index. If there is no saddle point in 'M', then 'indices' is the empty array.
+
+function [indices] = saddle(M)
+    
+    numel(Z(:,1))
+    numel(Z(1,:))
