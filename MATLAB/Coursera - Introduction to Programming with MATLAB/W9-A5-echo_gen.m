8c2c06df4c99a4cd4d629deff1c5c886c6bb6dfb
Loren Aran <52772608+marshmakie@users.noreply.github.com>
2020-09-09 16:21:12 -0300

    Create W9-A5-echo_gen.m

---
+++ MATLAB/Coursera - Introduction to Programming with MATLAB/W9-A5-echo_gen.m
@@ -0,0 +1,27 @@
+%%% Week 09 - Final Assignment 3: Echo Generator
+% Add an echo to an audio file.
+
+% Write a function called 'echo_gen' that adds an echo effect to an audio recording.
+% An echo is the original signal delayed and attenuated.
+% You will first need to compute the echo and then add it to the original signal with the correct offset.
+% The function is to be called like this:  output = echo_gen(input, fs, delay, amp);
+% where 'input' is a column vector with values between -1 and 1 representing a time series of digitalized sound data.
+% The input argument 'fs' is the sampling rate. The sampling rate specifies how many samples we have in the data each second.
+% For example: an audio CD uses 44,100 samples per second (44,1 kHz).
+% The input argument 'delay' represent the delay of the echo in seconds.
+% That is, the echo should start after 'delay' seconds have passed from the stard of the audio signal.
+% Finally, 'amp' specifies the amplification of the echo. While typically this is a value less than 1
+% because the echo is not as loud, your function should work if 'amp' is greater than 1.
+
+% The output of the function is a column vector containing the original sound with the echo superimposed.
+% The output vector will be longer than the input vector if the delay is not zero.
+% Round to the nearest number of points needed to get the delay, as opposed to floor or ceil.
+% A sound recording has values between -1 and 1, so if the echo causes some values to be outside of this range,
+% you will need to scale the entire vector, so that all values are within the range while retaining their relative amplitudes.
+
+% MATLAB has several sample audio files included that you can try, like 'splat', 'gong' and 'handel'.
+% Try the following:
+%   load gong % loads two variables, y and Fs
+%   sound(y,Fs) % outputs sound
+
+% (Note that we are assuming mono audiofiles. If the audio data has two columns, it is a stereo file, so use only one column of the data when testing your file.)
